6a0a0a0397b7e9d2e88170159ff0c9472825f84b
Mateus <mateusgms@icloud.com>
2016-05-10 12:34:14 -0300

    corrigido bug de numero de ingressos solicitados igual ao numero de ingressos disponiveis

--- src/test/java/br/com/caelum/agiletickets/models/SessaoTest.java
+++ src/test/java/br/com/caelum/agiletickets/models/SessaoTest.java
@@ -37,5 +37,12 @@ public class SessaoTest {
 		sessao.reserva(3);
 		Assert.assertEquals(2, sessao.getIngressosDisponiveis().intValue());
 	}
+	@Test
+	public void reservarOMesmoNumeroDeIngrssosDisponiveis() throws Exception {
+		Sessao sessao = new Sessao();
+		sessao.setTotalIngressos(2);
+
+		Assert.assertTrue(sessao.podeReservar(2));
+	}
 	
 }
